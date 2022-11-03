82c8c673d0b51e6bebd60c01ab9464b6faf6be8b
jihoseo2006 <jihoseo2006@gmail.com>
2022-11-03 10:30:25 +0900

    File clean
    파일정리

---
+++ Dodge/Assets/Scripts/Player.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody playerRigidbody; // 이동시 사용하는 리지드바디 컴포넌트
+    public float Movespeed = 12f;
+    
+    void Start()
+    {
+        playerRigidbody = GetComponent<Rigidbody>();
+    }
+
+   
+    void Update()
+    {
+        float xInput = Input.GetAxis("Horizontal");
+        float zInput = Input.GetAxis("Vertical");
+
+        float xSpeed = xInput * Movespeed;
+        float zSpeed = zInput * Movespeed;
+
+        Vector3 newVelocity = new Vector3(xSpeed, 0f, zSpeed);
+
+        playerRigidbody.velocity = newVelocity;
+    }
+
+    public void Die()
+    {
+        gameObject.SetActive(false);
+    }
+}
